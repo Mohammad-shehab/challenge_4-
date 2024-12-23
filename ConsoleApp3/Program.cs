86e2479247a9903b205bc219c33568aa2989a909
Mohammad-shehab <q8mlg@outlook.com>
2024-12-23 11:15:45 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System;
+
+class MainClass
+{
+
+    public static int OneDecremented(int num)
+    {
+        int count = 0;
+        string x = Convert.ToString(num);
+
+        for (int i =1;  i<   x.Length; i++)
+        {
+            if (x[i -1]-1 == x[i])
+                count++;
+           
+        }
+  
+
+
+
+        return count;
+
+    }
+
+    static void Main()
+    {
+
+        // keep this function call here
+        Console.WriteLine(OneDecremented(987654321));
+
+    }
+
+}
